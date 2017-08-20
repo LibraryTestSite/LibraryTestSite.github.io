66294a948caa4be37d781ac1d0b3dab611e2c5c8
LibraryTestSite <LibraryTestSite@users.noreply.github.com>
2017-08-20 09:34:20 -0700

    Create springShareRSS1.js
    The script used to generate RSS feed in SpringShare for a particular widget

---
+++ tasks/taskA1/springShareRSS1.js
@@ -0,0 +1,17 @@
+
+                    function loadRSS34752548(url){
+                        var script = document.createElement("script")
+                        script.type = "text/javascript";
+                        if (script.readyState){  //IE
+                            script.onreadystatechange = function(){
+                                if (script.readyState == "loaded" ||
+                                        script.readyState == "complete"){
+                                    script.onreadystatechange = null;
+                                }
+                            };
+                        }
+                        script.src = url;
+                        script.onload = function() { try { springSpace.UI.initPopOvers(); } catch(e) {}}; // Re-init popovers since they can load after page load.
+                        document.body.appendChild(script);
+                    };
+                
